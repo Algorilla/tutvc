856ed41a8a649a488452ef6a6e4b30a2cbdda4f9
Dmitri Novikov <dimanovi@hotmail.com>
2014-09-27 15:44:17 -0400

    Improved fish counter

--- count-fish.py
+++ count-fish.py
@@ -1,15 +1,17 @@
 # Add up the number of creatures in a data file
+import sys
+
+all_filenames = sys.argv[1:]
 total = 0
-reader = open('dmitri-2014.txt', 'r')
-for line in reader:
-	if line.startswith('#'):
-		pass
-	elif line.startswith('Date'):
-		pass
-	else:
-		temp = line.strip()
-		fields = temp.split(',')
-		count = int(fields[2])
-		total = total + count
-reader.close()
+for filename in all_filenames:
+	reader = open(filename, 'r')
+	for line in reader:
+		if line.startswith('#') or line.startswith('Date'):
+			pass
+		else:
+			temp = line.strip()
+			fields = temp.split(',')
+			count = int(fields[2])
+			total = total + count
+	reader.close()
 print('total number of creatures seen', total)
