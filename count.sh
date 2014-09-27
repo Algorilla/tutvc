4b7038bb18eae8a48e8429568cfaf378c20c9397
Dmitri <dym.novikov@gmail.com>
2014-09-27 00:56:00 -0400

    Importing files from shell tutorial

---
+++ count.sh
@@ -0,0 +1,8 @@
+# Display the name of each filename file and count of
+# the number of distinct occurences of each species
+# in that file
+for filename in data/*.txt
+do
+    echo $filename
+    grep -v Species $filename | cut -d , -f 2 | sort | uniq -c
+done
