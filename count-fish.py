a0f9721b6abfd2b5ba5b0caf5f63ea2795b64119
Dmitri Novikov <dimanovi@hotmail.com>
2014-09-27 15:24:22 -0400

    Added conditionals to count-fish.txt

--- count-fish.py
+++ count-fish.py
@@ -1,7 +1,15 @@
 # Add up the number of creatures in a data file
-reader = open('temp.txt', 'r')
+total = 0
+reader = open('dmitri-2014.txt', 'r')
 for line in reader:
-	temp = line.strip()
-	fields = temp.split(',')
-	print(fields)
+	if line.startswith('#'):
+		pass
+	elif line.startswith('Date'):
+		pass
+	else:
+		temp = line.strip()
+		fields = temp.split(',')
+		count = int(fields[2])
+		total = total + count
 reader.close()
+print('total number of creatures seen', total)
