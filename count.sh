9caf988bead2fb82fd4154b287563df4c8fdb6e1
Captain Obvious <duh@obvious.com>
2014-09-27 12:53:38 -0400

    Replacing $filename with $f
    I believe that $filename is too obvious

--- count.sh
+++ count.sh
@@ -1,9 +1,9 @@
 # Display the name of each data file and count of
 # the number of distict occurences of each species
 # in that file
-for filename in data/*.txt
+for f in data/*.txt
 do
     date
-    echo $filename
-    grep -v Species $filename | cut -d , -f 2 | sort | uniq -c | sort -n -r
+    echo $f
+    grep -v Species $f | cut -d , -f 2 | sort | uniq -c | sort -n -r
 done
